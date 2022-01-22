a5818422e258e6ad52a07ea8a5454b4895f152ad
Serionus <68148246+Serionus@users.noreply.github.com>
2022-01-22 12:23:09 +0100

    init

---
+++ src/main/kotlin/com/bydlak/bydlakapi/BydlakApiApplication.kt
@@ -0,0 +1,11 @@
+package com.bydlak.bydlakapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BydlakApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BydlakApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/bydlak/bydlakapi/BydlakApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bydlak.bydlakapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BydlakApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
