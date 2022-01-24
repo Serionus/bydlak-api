3bfb802923cda2b36ecf17e442c2f44a305ab24f
Serionus <68148246+Serionus@users.noreply.github.com>
2022-01-24 15:07:42 +0100

    int overflow

--- src/main/kotlin/com/bydlak/bydlakapi/alarm/Alarm.kt
+++ src/main/kotlin/com/bydlak/bydlakapi/alarm/Alarm.kt
@@ -3,7 +3,7 @@ package com.bydlak.bydlakapi.alarm
 import java.time.LocalDateTime
 
 data class Alarm(
-    val id: Int,
+    val id: Long,
     val title: String,
     val hour: LocalDateTime,
     val recurring: HashMap<Day, Boolean>
